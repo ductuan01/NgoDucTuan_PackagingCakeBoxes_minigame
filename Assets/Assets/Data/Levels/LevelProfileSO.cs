161ce708695c0eeb4e85e0ced51d3f93598d0301
ductuan01 <154719782+ductuan01@users.noreply.github.com>
2024-03-24 01:32:55 +0700

    first commit

--- Assets/Assets/Data/Levels/LevelProfileSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "LevelsProfileSO", menuName = "SO/LevelsProfileSO" )]
-public class LevelProfileSO : ScriptableObject
-{
-    public List<LevelInfo> levelInfos;
-}
